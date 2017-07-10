41a6f616a6ae954943f855d466881122731b473e
cornhundred <nickfloresfernandez@gmail.com>
2017-07-10 10:58:12 -0400

    adding notes and testing aws-eb deployment using zipped file again

--- application.py
+++ application.py
@@ -108,7 +108,7 @@ welcome = """
 <body id="sample">
   <div class="textColumn">
     <h1>Nick!!!!</h1>
-    <p>Your Docker Container is now running in Elastic Beanstalk on your own dedicated environment in the AWS Cloud.</p>
+    <p>Adding notes and working on local deployment.</p>
   </div>
 
   <div class="linksColumn">
